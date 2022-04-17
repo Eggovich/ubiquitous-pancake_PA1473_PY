f612cd90e81777fcd186ee733fa1cd3ac00d1aa0
OlofSamuelsson <olof.samuelsson00@gmail.com>
2022-04-17 10:06:19 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -67,6 +67,8 @@ def pick_up_fail_detection():
 def pick_up_(left_lift, right_lift):
     pass
 
+# function used inside line_follow to determine the line it should follow. 
+# Must have a variabel that sends the correct line to follow depending on where we want the robot to go.
 def color_line_follow():
     for color in possible_colors:
         if color == color.YELLOW:
