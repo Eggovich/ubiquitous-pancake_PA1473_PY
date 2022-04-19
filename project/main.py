db9ef987fc7f0adf0c8fb4c68b37568bf22ed722
OlofSamuelsson <olof.samuelsson00@gmail.com>
2022-04-19 10:55:31 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -4,7 +4,7 @@
 import sys
 
 
-from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Port,  Direction, stop, color
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
@@ -12,29 +12,37 @@ from pybricks.robotics import DriveBase
 # GLOBALA VARIABLES
 
 # Motor (Korrigera portarna då dessa stämmer inte)
-left_motor = Motor(Port.B)
-right_motor = Motor(Port.C)
+left_motor = Motor(Port.C)
+right_motor = Motor(Port.B)
 
-left_lift = Motor(Port.D)
-right_lift = Motor(Port.A)
+Crane_motor = Motor(Port.A, positive_direction=Direction.CLOCKWISE, gears = [12, 36])
+
+Front_button=Port.S1
+
+Ultrasonic_sensor = UltrasonicSensor(Port.S4)
 
 # Color sensor.
-line_sensor = ColorSensor(Port.S3)
+Color_sensor = ColorSensor(Port.S3)
 
 # All the colors
-possible_colors = [color.RED, color.GREEN, color.BLUE, color.BROWN, color.YELLOW]
+possible_colors = [color.RED, color.GREEN, color.BLUE, color.BROWN, color.YELLOW, color.BLACK, color.WHITE]
 
-# Drive base.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+# Drive base. #wheel_diameter= 47, axle_track= 128
+robot = DriveBase(left_motor, right_motor, wheel_diameter = 47, axle_track = 128)
 
-# The logic under
-def line_follow (robot, line_sensor, left_motor, right_motor): # måste ändra till våra värden!!!!!!!!
+# ---------------------------The logic under-----------------------
+
+def line_follow (robot): # måste ändra till våra värden!!!!!!!!
     # Initialize the motors.
+    
+    # Calculate the color
+    
+    color = color_detection()
+    # Calculate the light threshold. Choose values based on your measurements.
+    WHITE = 85 #not our value
+    threshold = (color + WHITE) / 2
 
-# Calculate the light threshold. Choose values based on your measurements.
-    BLACK = 9
-    WHITE = 85
-    threshold = (BLACK + WHITE) / 2
+   
 
     # Set the drive speed at 100 millimeters per second.
     DRIVE_SPEED = 100
@@ -50,7 +58,7 @@ def line_follow (robot, line_sensor, left_motor, right_motor): # måste ändra t
     # Start following the line endlessly.
     while True:
         # Calculate the deviation from the threshold.
-        deviation = line_sensor.reflection() - threshold
+        deviation = Color_sensor.reflection() - threshold
 
         # Calculate the turn rate.
         turn_rate = PROPORTIONAL_GAIN * deviation
@@ -61,41 +69,51 @@ def line_follow (robot, line_sensor, left_motor, right_motor): # måste ändra t
         # You can wait for a short time or do other things in this loop.
         wait(10)
 
-def pick_up_fail_detection():
-    pass
+def pick_up_pallet(Crane_motor, Front_button):
+    try: 
+        if Front_button.pressed() == True:
+         Crane_motor.run_untill_stalled(50, then = stop.HOLD, duty_limit = 60)
+        else:
+            pick_up_fail_detection()
+    except:
+        print("Another problem")
+        
+    
 
-def pick_up_(left_lift, right_lift):
-    pass
+def pick_up_fail_detection(Crane_motor, Front_button):
+    try:
+        if Front_button.pressed() == False:
+            print("Pick up fail detection noticed")
+            Crane_motor.run_untill_stalled(-50, then = stop, duty_limit = None)
+            return False
+        else:
+            pass
+    except:
+        print("something went wrong")
 
 # Function determining which way it should go
 def direction():
-
+    pass
 
 # function used inside line_follow to determine the line it should follow. 
 # Must have a variabel that sends the correct line to follow depending on where we want the robot to go.
-def color_line_follow():
-    route_choice = 
-    for color in possible_colors:
-        while color == color.YELLOW:
-            line_follow()
-        while color == color.RED:
-            pass
-        while color == color.BLUE:
-            pass
-        while color == color.BROWN:
-            pass
-        while color == color.GREEN:
-            pass
+def color_detection():
+    color = color()
+    return color
+        
+            
+
+
+def road_choice():
     pass
 
 
-
-# over
+# ---------------------------- Main funktion ----------------------------------
 
 def main():
-    
-    return 0
+    line_follow(robot)
 
 
+# ---------------------------- Ignore this ------------------------------------
 if __name__ == '__main__':
     sys.exit(main())
